8412070675db62e782fe6aca85af49a745828eba
Igor Sh <igar15@yandex.ru>
2020-10-06 21:59:37 +0300

    Add @CrossOrigin("localhost:4200") to ProductCategoryRepository and ProductRepository

--- src/main/java/com/igar15/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/igar15/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.igar15.ecommerce.dao;
 import com.igar15.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category") //override default name of JSON entry and default rest path
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
