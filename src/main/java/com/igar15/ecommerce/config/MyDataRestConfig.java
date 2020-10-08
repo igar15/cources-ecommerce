63c21b5c722094b02467f109a25ddaffe014e47a
Igor Sh <igar15@yandex.ru>
2020-10-08 14:19:14 +0300

    Add exposeIds method to MyDataRestConfig

--- src/main/java/com/igar15/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/igar15/ecommerce/config/MyDataRestConfig.java
@@ -2,13 +2,25 @@ package com.igar15.ecommerce.config;
 
 import com.igar15.ecommerce.entity.Product;
 import com.igar15.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Autowired
+    private EntityManager entityManager;
+
+
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
@@ -16,17 +28,37 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 
 
-        //disable HTTP methods for Product: POST, PUT, DELETE
+        // disable HTTP methods for Product: POST, PUT, DELETE
         config.getExposureConfiguration()
                 .forDomainType(Product.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
 
-        //disable HTTP methods for ProductCategory: POST, PUT, DELETE
+        // disable HTTP methods for ProductCategory: POST, PUT, DELETE
         config.getExposureConfiguration()
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
 
+        // call an internal helper method
+        exposeIds(config);
     }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        // get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // create an array list of the entity types
+        List<Class<?>> entityClasses = new ArrayList<>();
+
+        // get the entity types for the entities
+        for (EntityType entityType : entities) {
+            entityClasses.add(entityType.getJavaType());
+        }
+
+        //expose the entity ids for the array of entity/domain types
+        Class[] classes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(classes);
+    }
+
 }
