dc0371ef0fef382171fe63738ad29c52d7d11d7d
Igor Sh <igar15@yandex.ru>
2020-10-12 10:47:52 +0300

    Add showSql=true property to application.properties; Add @EntityGraph("country") to findByNameContaining method in ProductRepository

--- src/main/java/com/igar15/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/igar15/ecommerce/dao/ProductRepository.java
@@ -3,6 +3,7 @@ package com.igar15.ecommerce.dao;
 import com.igar15.ecommerce.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -13,5 +14,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByCategoryId(Long id, Pageable pageable);
 
+    @EntityGraph(attributePaths="category")
     Page<Product> findByNameContaining(String name, Pageable pageable);
 }
